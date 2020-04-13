5e6b00155b1927707af78e178032243193e62a0f
matthew-kaye <matthew@brainlabsdigital.com>
2020-04-13 01:32:39 +0100

    update and expand damage calculator

--- kaye_tech/frontend/src/pages/Tabletop.vue
+++ kaye_tech/frontend/src/pages/Tabletop.vue
@@ -8,9 +8,9 @@
       <v-row>
         <v-col cols="1" sm="1">
           <v-select
-            v-model="characterLevel"
+            v-model="character.level"
             :rules="requiredField"
-            :items="getLevels()"
+            :items="getNumberArray(1, 20)"
             required
             attach
             label="Character Level"
@@ -19,7 +19,7 @@
         </v-col>
         <v-col cols="2" sm="2">
           <v-select
-            v-model="characterClass"
+            v-model="character.class"
             :rules="requiredField"
             :items="classes"
             required
@@ -30,7 +30,7 @@
         </v-col>
         <v-col cols="2" sm="2">
           <v-select
-            v-model="fightingStyle"
+            v-model="character.fightingStyle"
             :rules="requiredField"
             :items="fightingStyles"
             required
@@ -39,8 +39,11 @@
             :menu-props="{ transition: 'slide-y-transition' }"
           ></v-select>
         </v-col>
-        <v-col cols="1" sm="1">
+        <v-col cols="2" sm="2">
           <v-switch v-model="advantage" class="ma-2" label="Advantage"></v-switch>
+        </v-col>
+        <v-col cols="2" sm="2">
+          <v-switch v-model="magic" class="ma-2" label="+1 Weapon"></v-switch>
         </v-col>
       </v-row>
     </v-card-text>
@@ -48,19 +51,37 @@
     <v-card-text>
       <v-row>
         <v-col cols="1" sm="1">
-          <v-text-field v-model="proficiencyBonus" label="Proficiency" required></v-text-field>
+          <v-select
+            v-model="proficiencyBonus"
+            :items="getNumberArray(2, 6)"
+            attach
+            label="Proficiency"
+            :menu-props="{ transition: 'slide-y-transition' }"
+          ></v-select>
         </v-col>
         <v-col cols="2" sm="2">
-          <v-text-field v-model="attackStat" label="Attack Stat" required></v-text-field>
+          <v-select
+            v-model="attackStat"
+            :items="getNumberArray(1, 5)"
+            attach
+            label="Attack Stat"
+            :menu-props="{ transition: 'slide-y-transition' }"
+          ></v-select>
         </v-col>
         <v-col cols="2" sm="2">
-          <v-text-field v-model="attackDice" label="Attack Dice (base)" required></v-text-field>
+          <v-text-field v-model="displayDice" label="Attack Dice (base)" required></v-text-field>
         </v-col>
         <v-col cols="1" sm="1">
           <v-text-field v-model="averageAC" label="Est. AC" required></v-text-field>
         </v-col>
         <v-col cols="1" sm="1">
-          <v-text-field v-model="numberOfAttacks" label="Attacks" required></v-text-field>
+          <v-select
+            v-model="numberOfAttacks"
+            :items="getNumberArray(1,4)"
+            attach
+            label="Attacks"
+            :menu-props="{ transition: 'slide-y-transition' }"
+          ></v-select>
         </v-col>
       </v-row>
     </v-card-text>
@@ -71,108 +92,136 @@
 <script>
 export default {
   components: {},
-  created() {},
+  created() {
+    this.calculateFields();
+  },
   data() {
     return {
-      characterLevel: 1,
+      character: {
+        level: 1,
+        class: "Fighter",
+        fightingStyle: "Duelling"
+      },
       requiredField: [v => !!v],
-      characterClass: "",
       classes: ["Fighter"],
       fightingStyles: ["Duelling", "Two-Handed", "Two-Weapon", "Archery"],
-      fightingStyle: "",
       advantage: false,
-      ACOverride: false,
       attackDamage: 0,
-      bonusAttackDamage: 0
+      bonusAttackDamage: 0,
+      averageAC: 14,
+      attackStat: 3,
+      attackBonus: 3,
+      averageDamageDie: 6,
+      proficiencyBonus: 2,
+      numberOfAttacks: 1,
+      extraDamage: 0,
+      critChance: 0.05,
+      displayDice: "",
+      magic: false
     };
   },
   computed: {
-    proficiencyBonus: {
-      get() {
-        return Math.ceil(this.characterLevel / 4) + 1;
-      },
-      set(value) {
-        return parseInt(value);
-      }
-    },
-    attackStat: {
-      get() {
-        var baseStat = Math.min(Math.floor(this.characterLevel / 4) + 3, 5);
-        if (this.characterClass == "Fighter") {
-          baseStat = this.characterLevel > 5 ? 5 : baseStat;
-        }
-        return this.fightingStyle == "Archery" ? baseStat + 2 : baseStat;
-      },
-      set(value) {
-        return parseInt(value);
-      }
-    },
-    attackDice: {
-      get() {
-        switch (this.fightingStyle) {
-          case "Duelling":
-            this.attackDamage = 6.5 + this.attackStat;
-            return "d8 + 2";
-          case "Two-Handed":
-            this.attackDamage = 3.5 + this.attackStat;
-            this.bonusAttackDamage = this.attackDamage;
-            return "d6 + d6";
-          case "Archery":
-            this.attackDamage = 4.5 + this.attackStat;
-            return "d8";
-          case "Two-Weapon":
-            this.attackDamage = 7 + this.attackStat;
-            return "2d6";
-        }
-      },
-      set(value) {
-        return value;
-      }
-    },
-    averageAC: {
-      get() {
-        if (!this.ACOverride) {
-          return Math.ceil(this.characterLevel / 3) + 13;
-        }
-      },
-      set(value) {
-        return value;
-      }
-    },
-    numberOfAttacks: {
-      get() {
-        if (this.characterLevel == 20) {
-          return 4;
-        } else if (this.characterLevel > 10) {
-          return 3;
-        } else if (this.characterLevel > 4) {
-          return 2;
-        }
-        return 1;
-      },
-      set(value) {
-        return parseInt(value);
-      }
-    },
     damage() {
-      var toHit = this.proficiencyBonus + this.attackStat;
-      var averageDamage =
-        this.numberOfAttacks * this.attackDamage + this.bonusAttackDamage;
-      var chanceToHit = 1 - (this.averageAC - 1 - toHit) / 20;
+      this.attackBonus = this.magic ? this.attackStat + 1 : this.attackStat;
+      if (this.character.fightingStyle == "Archery") {
+        this.attackBonus = this.attackBonus + 2;
+      }
+      var toHit = this.proficiencyBonus + this.attackBonus;
+      var averageDamage = this.numberOfAttacks * this.attackDamage;
+      if (this.averageAC <= toHit) {
+        var chanceToHit = 1;
+      } else if (this.averageAC > 20 + toHit) {
+        var chanceToHit = 0.05;
+      } else {
+        var chanceToHit = 1 - (this.averageAC - 1 - toHit) / 20;
+      }
       chanceToHit = this.advantage
         ? 1 - Math.pow(1 - chanceToHit, 2)
         : chanceToHit;
-      return (chanceToHit * averageDamage).toFixed(1);
+      this.critChance = this.advantage
+        ? Math.pow(1 - this.critChance, 2)
+        : this.critChance;
+      var critDamage =
+        this.numberOfAttacks * this.critChance * this.averageDamageDie;
+      return (chanceToHit * averageDamage + critDamage).toFixed(1);
     }
   },
   methods: {
-    getLevels() {
-      var levels = Array.from(Array(21).keys());
-      levels.shift();
+    getNumberArray(start, end) {
+      var levels = Array.from(Array(end + 1).keys());
+      for (var x = 0; x < start; x++) {
+        levels.shift();
+      }
       return levels;
     },
-    getProficiencyFromLevel(level) {
-      return Math.ceil(level / 4) + 1;
+    calculateFields() {
+      this.proficiencyBonus = this.calculateProficiencyBonus();
+      this.attackStat = this.calculateAttackStat();
+      this.calculateAttackDice();
+      this.averageAC = this.calculateAverageAC();
+      this.numberOfAttacks = this.calculateNumberOfAttacks();
+    },
+    calculateProficiencyBonus() {
+      return Math.ceil(this.character.level / 4) + 1;
+    },
+    calculateAttackStat() {
+      var baseStat = Math.min(Math.floor(this.character.level / 4) + 3, 5);
+      if (this.character.class == "Fighter") {
+        return this.character.level > 5 ? 5 : baseStat;
+      }
+      return baseStat;
+    },
+    calculateAttackDice() {
+      this.extraDamage = 0;
+      switch (this.character.fightingStyle) {
+        case "Duelling":
+          this.averageDamageDie = 4.5;
+          this.extraDamage = 2;
+          this.displayDice = "d8 + 2";
+          break;
+        case "Two-Handed":
+          this.averageDamageDie = 4.5 + 3.5;
+          this.displayDice = "2d6";
+          break;
+        case "Archery":
+          this.averageDamageDie = 4.5;
+          this.displayDice = "d8";
+          break;
+        case "Two-Weapon":
+          this.averageDamageDie = 3.5;
+          this.displayDice = "d6 + d6";
+          break;
+      }
+      this.attackDamage = this.magic
+        ? this.averageDamageDie + this.extraDamage + this.attackStat
+        : this.averageDamageDie + this.extraDamage + this.attackStat + 1;
+    },
+    calculateAverageAC() {
+      return Math.ceil(this.character.level / 3) + 13;
+    },
+    calculateNumberOfAttacks() {
+      var bonusAttacks = this.character.fightingStyle == "Two-Weapon" ? 1 : 0;
+      switch (this.character.class) {
+        case "Fighter":
+          if (this.character.level == 20) {
+            return 4 + bonusAttacks;
+          } else if (this.character.level > 10) {
+            return 3 + bonusAttacks;
+          }
+          break;
+      }
+      if (this.character.level > 4) {
+        return 2 + bonusAttacks;
+      }
+      return 1 + bonusAttacks;
+    }
+  },
+  watch: {
+    character: {
+      deep: true,
+      handler() {
+        this.calculateFields();
+      }
     }
   }
 };
