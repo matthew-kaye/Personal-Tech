8cfccf46a0e9d95d07b169dc0b59e2b431904ce4
matthew-kaye <m.kaye@outlook.com>
2020-05-08 08:39:37 +0100

    lazy nav bar appearance

--- kaye_tech/frontend/src/components/NavigationBar.vue
+++ kaye_tech/frontend/src/components/NavigationBar.vue
@@ -1,56 +1,58 @@
 <template>
-  <div v-if="currentUser">
-    <v-navigation-drawer v-model="drawer" absolute temporary app>
-      <v-list>
-        <v-list-item v-for="item in navigationItems" :key="item.label" :to="item.link" link>
-          <v-list-item-icon>
-            <v-icon color="secondary">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>{{ item.label }}</v-list-item-title>
-        </v-list-item>
-        <v-list-item href="/accounts/logout/">
-          <v-list-item-icon>
-            <v-icon color="secondary">mdi-logout</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Log Out</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-lazy transition="scroll-y-transition">
+    <div v-if="currentUser">
+      <v-navigation-drawer v-model="drawer" absolute temporary app>
+        <v-list>
+          <v-list-item v-for="item in navigationItems" :key="item.label" :to="item.link" link>
+            <v-list-item-icon>
+              <v-icon color="secondary">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.label }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item href="/accounts/logout/">
+            <v-list-item-icon>
+              <v-icon color="secondary">mdi-logout</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Log Out</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-    <v-app-bar dark color="primary">
-      <v-app-bar-nav-icon class="hidden-lg-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <router-link to="/" tag="span" style="cursor: pointer; display: flex;">
-        <v-img class="ml-2" max-height="50" max-width="60" src="/static/frontend/k-logo.jpg" />
-        <v-toolbar-title class="ma-3">
-          <b class="headline font-weight-bold">Matt's Tech</b>
-        </v-toolbar-title>
-      </router-link>
-      <v-spacer></v-spacer>
-      <v-tooltip bottom color="tooltip">
-        <template v-slot:activator="{ on }">
-          <v-btn icon text @click="toggleDark" v-on="on">
-            <v-icon>mdi-brightness-6</v-icon>
-          </v-btn>
-        </template>
-        Toggle dark mode
-      </v-tooltip>
-      <v-toolbar-items class="hidden-md-and-down">
-        <v-chip class="ma-4 pa-0" outlined color="white">
-          <v-btn text v-for="item in navigationItems" :key="item.label" :to="item.link">
-            <v-icon left dark>{{ item.icon }}</v-icon>
-            {{ item.label }}
-          </v-btn>
-        </v-chip>
-      </v-toolbar-items>
-      <v-avatar class="ml-2" size="40" v-if="currentUser.profile.avatar">
-        <img :src="currentUser.profile.avatar" />
-      </v-avatar>
-      <v-btn class="ml-2 mr-1" icon text href="/accounts/logout/" v-if="currentUser.id">
-        <v-icon dark>mdi-logout</v-icon>
-      </v-btn>
-      <v-btn class="ml-2 mr-1" text href="/accounts/splash/" v-if="!currentUser.id">Sign In</v-btn>
-    </v-app-bar>
-  </div>
+      <v-app-bar dark color="primary">
+        <v-app-bar-nav-icon class="hidden-lg-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <router-link to="/" tag="span" style="cursor: pointer; display: flex;">
+          <v-img class="ml-2" max-height="50" max-width="60" src="/static/frontend/k-logo.jpg" />
+          <v-toolbar-title class="ma-3">
+            <b class="headline font-weight-bold">Matt's Tech</b>
+          </v-toolbar-title>
+        </router-link>
+        <v-spacer></v-spacer>
+        <v-tooltip bottom color="tooltip">
+          <template v-slot:activator="{ on }">
+            <v-btn icon text @click="toggleDark" v-on="on">
+              <v-icon>mdi-brightness-6</v-icon>
+            </v-btn>
+          </template>
+          Toggle dark mode
+        </v-tooltip>
+        <v-toolbar-items class="hidden-md-and-down">
+          <v-chip class="ma-4 pa-0" outlined color="white">
+            <v-btn text v-for="item in navigationItems" :key="item.label" :to="item.link">
+              <v-icon left dark>{{ item.icon }}</v-icon>
+              {{ item.label }}
+            </v-btn>
+          </v-chip>
+        </v-toolbar-items>
+        <v-avatar class="ml-2" size="40" v-if="currentUser.profile.avatar">
+          <img :src="currentUser.profile.avatar" />
+        </v-avatar>
+        <v-btn class="ml-2 mr-1" icon text href="/accounts/logout/" v-if="currentUser.id">
+          <v-icon dark>mdi-logout</v-icon>
+        </v-btn>
+        <v-btn class="ml-2 mr-1" text href="/accounts/splash/" v-if="!currentUser.id">Sign In</v-btn>
+      </v-app-bar>
+    </div>
+  </v-lazy>
 </template>
 
 <script>
--- kaye_tech/frontend/static/script.js
+++ kaye_tech/frontend/static/script.js
@@ -22,9 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-
-
-
 console.log(window.hasOwnProperty("canvas"))
 const canvas = document.getElementsByTagName('canvas')[0]
 resizeCanvas();
