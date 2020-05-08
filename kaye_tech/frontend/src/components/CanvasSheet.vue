9eb712a164a03459bf44f39b34b6ca8be66628d1
matthew-kaye <m.kaye@outlook.com>
2020-05-08 04:31:45 +0100

    canvas page and components

---
+++ kaye_tech/frontend/src/components/CanvasSheet.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CanvasSheet",
+  created() {
+    window.ga =
+      window.ga ||
+      function() {
+        (ga.q = ga.q || []).push(arguments);
+      };
+    ga.l = +new Date();
+    ga("create", "UA-105392568-1", "auto");
+    ga("send", "pageview");
+  },
+  mounted() {
+    const guiPlugin = document.createElement("script");
+    guiPlugin.setAttribute("src", "/static/dat.gui.min.js");
+    guiPlugin.async = true;
+    document.head.appendChild(guiPlugin);
+
+    const scriptPlugin = document.createElement("script");
+    scriptPlugin.setAttribute("src", "/static/script.js");
+    scriptPlugin.async = true;
+    document.head.appendChild(scriptPlugin);
+  }
+};
+</script>
+<style module lang="scss">
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ kaye_tech/frontend/src/pages/Canvas.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="ma-6" height="100%">
+    <v-card-title class="primary headline">
+      <span class="white--text">Canvas</span>
+
+      <v-btn color="primary" class="ml-8 mr-1" @click="refresh()">Refresh</v-btn>
+    </v-card-title>
+    <div width="100%" height="100%" class="ma-10">
+      <CanvasSheet />
+      <v-divider />
+      <br />
+      <v-card md="auto" elevation="10" width="20%">
+        <v-card-title class="primary headline">Instructions</v-card-title>
+        <v-divider />
+        <v-card-text style="font-size:12pt">Press 'p' to pause</v-card-text>
+      </v-card>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import CanvasSheet from "@/components/CanvasSheet.vue";
+
+export default {
+  components: {
+    CanvasSheet
+  },
+  created() {},
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    refresh() {
+      window.location.reload();
+    }
+  }
+};
+</script>
