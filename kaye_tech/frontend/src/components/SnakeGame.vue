e70b4c3c125b55f789523920291c76a8f2221b4c
matthew-kaye <m.kaye@outlook.com>
2020-06-01 23:58:34 +0100

    remove infinite loop

--- kaye_tech/frontend/src/components/SnakeGame.vue
+++ kaye_tech/frontend/src/components/SnakeGame.vue
@@ -137,26 +137,21 @@ export default {
         active = true;
         requestAnimationFrame(loop);
       }
-      updatePlannedMove();
-      function updatePlannedMove() {
-        if (moved) {
-          if (e.which === 37 && snake.dx === 0) {
-            snake.dx = -grid;
-            snake.dy = 0;
-          } else if (e.which === 38 && snake.dy === 0) {
-            snake.dy = -grid;
-            snake.dx = 0;
-          } else if (e.which === 39 && snake.dx === 0) {
-            snake.dx = grid;
-            snake.dy = 0;
-          } else if (e.which === 40 && snake.dy === 0) {
-            snake.dy = grid;
-            snake.dx = 0;
-          }
-          moved = false;
-        } else {
-          updatePlannedMove();
+      if (moved) {
+        if (e.which === 37 && snake.dx === 0) {
+          snake.dx = -grid;
+          snake.dy = 0;
+        } else if (e.which === 38 && snake.dy === 0) {
+          snake.dy = -grid;
+          snake.dx = 0;
+        } else if (e.which === 39 && snake.dx === 0) {
+          snake.dx = grid;
+          snake.dy = 0;
+        } else if (e.which === 40 && snake.dy === 0) {
+          snake.dy = grid;
+          snake.dx = 0;
         }
+        moved = false;
       }
     });
     if (!freezeOverride) requestAnimationFrame(loop);
