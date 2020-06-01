27f47e2d86a34bd464b1e3471cd74b0f655c49a8
matthew-kaye <m.kaye@outlook.com>
2020-06-01 02:57:53 +0100

    re-add comma I accidentally deleted and hope that fixes everything

--- kaye_tech/backend/urls.py
+++ kaye_tech/backend/urls.py
@@ -4,7 +4,7 @@ from backend import views
 
 router = routers.DefaultRouter()
 router.register(
-    "burritos", views.BurritoViewSet, basename="burritos_base"
+    "burritos", views.BurritoViewSet, basename="burritos_base",
 )
 router.register("snake", views.SnakeViewSet, basename="snake_base",)
 
