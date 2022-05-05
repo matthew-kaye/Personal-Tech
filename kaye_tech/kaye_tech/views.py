a7506325b665db5227d6f099df3c8600e49436fb
matthew-kaye <matthew@brainlabsdigital.com>
2022-05-05 15:43:11 +0100

    reinstate google auth and update requirements

--- kaye_tech/kaye_tech/views.py
+++ kaye_tech/kaye_tech/views.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 
-# @method_decorator(login_required(login_url="/accounts/splash"), name="dispatch")
+@method_decorator(login_required(login_url="/accounts/splash"), name="dispatch")
 class IndexView(TemplateView):
     template_name = "index.html"
